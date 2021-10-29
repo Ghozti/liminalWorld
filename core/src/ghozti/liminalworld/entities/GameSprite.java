29784857675f5555c4af98fc5b3051461a41e3fa
Ghozti <ybalderas1005@gmail.com>
2021-10-29 09:10:08 -0400

    - created getters and setters for GameSprite

--- core/src/ghozti/liminalworld/entities/GameSprite.java
+++ core/src/ghozti/liminalworld/entities/GameSprite.java
@@ -3,6 +3,7 @@ package ghozti.liminalworld.entities;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Rectangle;
+import org.w3c.dom.Text;
 
 public class GameSprite {
 
@@ -13,19 +14,51 @@ public class GameSprite {
     float width, height;
     Sprite sprite;
 
-    public GameSprite(Texture texture, Rectangle rectangle, float[] position, float width, float height, Sprite sprite){
+    public void setTexture(Texture texture){
         this.texture = texture;
+    }
+
+    public void setHitbox(Rectangle rectangle){
         this.hitbox = rectangle;
+    }
+
+    public void setPosition(float[] position){
         this.position = position;
+    }
+
+    public void setWidth(float width){
         this.width = width;
+    }
+
+    public void setHeight(float height){
         this.height = height;
+    }
 
-        hitbox.x = position[0];
-        hitbox.y = position[1];
-        hitbox.width = width;
-        hitbox.height = height;
-
+    public void createSpite(Sprite sprite){
         this.sprite = sprite;
     }
 
+    public Texture getTexture() {
+        return texture;
+    }
+
+    public Rectangle getHitbox() {
+        return hitbox;
+    }
+
+    public float[] getPosition() {
+        return position;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public Sprite getSprite() {
+        return sprite;
+    }
 }
