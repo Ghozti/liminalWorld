8a2065481e89e92ae90646f737a58f7d1c67205b
Ghozti <ybalderas1005@gmail.com>
2021-11-06 19:35:07 -0400

    - filled in EntitySpawner

--- core/src/ghozti/liminalworld/entities/spawners/CometSpawner.java
+++ core/src/ghozti/liminalworld/entities/spawners/CometSpawner.java
@@ -1,4 +1,20 @@
 package ghozti.liminalworld.entities.spawners;
 
-public class CometSpawner {
+import com.badlogic.gdx.graphics.g2d.Batch;
+
+public class CometSpawner extends EntitySpawner{
+    @Override
+    public void drawEntities(Batch batch) {
+
+    }
+
+    @Override
+    public void entityOverlaps() {
+
+    }
+
+    @Override
+    public void disposeEntities() {
+
+    }
 }
--- core/src/ghozti/liminalworld/entities/spawners/EntitySpawner.java
+++ core/src/ghozti/liminalworld/entities/spawners/EntitySpawner.java
@@ -1,4 +1,17 @@
 package ghozti.liminalworld.entities.spawners;
 
-public interface EntitySpawner {
+import com.badlogic.gdx.graphics.g2d.Batch;
+import ghozti.liminalworld.entities.GameSprite;
+
+import java.util.ArrayList;
+
+public abstract class EntitySpawner {
+
+    ArrayList<GameSprite> entityList;
+
+    public abstract void drawEntities(Batch batch);
+
+    public abstract void entityOverlaps();
+
+    public abstract void disposeEntities();
 }
